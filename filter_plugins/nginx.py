39d3f5f06abc7de474aac987f4e27c3a2af91f40
Emilien M <HanXHX@users.noreply.github.com>
2019-04-26 13:29:06 +0200

    Fix lints warnings (#40)
    * Add new filter plugins related to SSL
    * Ignore lint on few tasks

--- filter_plugins/nginx.py
+++ filter_plugins/nginx.py
@@ -10,11 +10,29 @@ def nginx_site_name(site):
     else:
         return site['name']
 
+def nginx_ssl_dir(pair, ssl_dir):
+    return ssl_dir + '/' + nginx_site_filename(pair)
+
+def nginx_key_path(pair, ssl_dir):
+    if pair.has_key('dest_key'):
+        return pair['dest_key']
+    else:
+        return nginx_ssl_dir(pair, ssl_dir) + '/' + nginx_site_filename(pair) + '.key'
+
+def nginx_cert_path(pair, ssl_dir):
+    if pair.has_key('dest_cert'):
+        return pair['dest_cert']
+    else:
+        return nginx_ssl_dir(pair, ssl_dir) + '/' + nginx_site_filename(pair) + '.crt'
+
 class FilterModule(object):
     ''' Nginx module '''
 
     def filters(self):
         return {
             'nginx_site_filename': nginx_site_filename,
-            'nginx_site_name': nginx_site_name
+            'nginx_site_name': nginx_site_name,
+            'nginx_ssl_dir': nginx_ssl_dir,
+            'nginx_key_path': nginx_key_path,
+            'nginx_cert_path': nginx_cert_path
         }
